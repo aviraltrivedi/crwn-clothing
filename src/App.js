86da310b5b0358b63a0e68516533f7b1126f39c4
aviral.trivedi <trivedi.aviral24@gmail.com>
2020-08-23 15:25:17 +0530

    Adding Route and Switch to navigate between pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
-class App extends Component{
-  render(){
-    return (
-      <div className='App'>
-        <HomePage/>
-      </div>
-    )
-  }
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+function App(){
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
